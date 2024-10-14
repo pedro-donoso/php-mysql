60935f8074cc33089f364b69d24d6701ac7dcebf
Pedro Donoso <pedro.donoso.r@usach.cl>
2024-10-13 23:46:29 -0300

    uno tablas

--- platillos.php
+++ platillos.php
@@ -19,38 +19,41 @@
 
 
     <div class="container">
-        <h2>Tabla de platillos</h2>
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>Precio</th>
-                    <th>Disponible</th>
-                    <th>Categoria ID</th>
-                </tr>
-            </thead>
-            <tbody>
-                <?php
-                require_once "./data/conexion.php";
+    <h2>Tabla de platillos</h2>
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Disponible</th>
+                <th>Categoria</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php
+            require_once "./data/conexion.php";
 
-                $sql = "SELECT * FROM newschema.platillos";
-                $resultados = $conexion->query($sql);
-                $resultados->execute();
+            $sql = "SELECT p.id, p.nombre, p.precio, p.disponible, c.nombre AS categoria 
+                    FROM newschema.platillos p 
+                    INNER JOIN newschema.categoria c ON p.categoriaId = c.id
+                    ORDER BY p.id ASC";
+            $resultados = $conexion->prepare($sql);
+            $resultados->execute();
 
-                while ($fila = $resultados->fetch(PDO::FETCH_OBJ)) {
-                    echo "<tr>";
-                    echo "<td>" . $fila->id . "</td>";
-                    echo "<td>" . $fila->nombre . "</td>";
-                    echo "<td>" . $fila->precio . "</td>";
-                    echo "<td>" . $fila->disponible . "</td>";
-                    echo "<td>" . $fila->categoriaId . "</td>";
-                    echo "</tr>";
-                }
-                ?>
-            </tbody>
-        </table>
-    </div>
+            while ($fila = $resultados->fetch(PDO::FETCH_OBJ)) {
+                echo "<tr>";
+                echo "<td>" . $fila->id . "</td>";
+                echo "<td>" . $fila->nombre . "</td>";
+                echo "<td>" . $fila->precio . "</td>";
+                echo "<td>" . $fila->disponible . "</td>";
+                echo "<td>" . $fila->categoria . "</td>";
+                echo "</tr>";
+            }
+            ?>
+        </tbody>
+    </table>
+</div>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js"></script>
